7af3fbc2c067345d24f6df7cafb9c5f18a0973f1
Lucas <lucas.bouvarel@gmail.com>
2019-11-09 16:22:27 +0100

    affichage d'une carte

--- prorientation/src/app/home/home.page.ts
+++ prorientation/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text="default text";
+  constructor(private router:Router) {}
+  onChangeText(){
+    this.text="changed"
+  }
+  go(){
+    this.router.navigate(['/map']);
+  }
 
-  constructor() {}
+
 
 }
---
+++ prorientation/src/app/map/map.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.page.html',
+  styleUrls: ['./map.page.scss'],
+})
+export class MapPage implements OnInit {
+  text="custom"
+  constructor() {
+  }
+  //var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  change(e){
+    this.text=e;
+  }
+
+  ngOnInit() {
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+ 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+ 	maxZoom: 18,
+ 	id: 'mapbox.streets',
+ 	accessToken: 'pk.eyJ1IjoibHVjYXNib3V2YXJlbCIsImEiOiJjazJycHIwbXQwZGs3M25udmltaGg3eTFlIn0.XGIAxbBH8QGE1ZnmHUztMQ'
+ }).addTo(mymap);
+  }
+
+}
