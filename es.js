c5ea7afcd04505c485fcee70032eba0664417584
Lucas Bouvarel <lucasbouvarel@macbook-pro-de-lucas-2.home>
2019-12-04 16:32:10 +0100

    avoir point d'interet en tableau

--- es.js
+++ es.js
@@ -1,34 +1,34 @@
-var add= (i)=>{
 
-  return new Promise(function(resolve,reject){
-    i=i+1;
-    if(i==1){
-      resolve(i);
+var request=require('request-promise');
+/*var options = {
+  uri : "https://www.overpass-api.de/api/interpreter",
+  qs : {
+    data : "[out:json];node[tourism](43.46669501043081,-5.708215989569187,43.588927989569186,-5.605835010430813);out%20meta;"
+  },
+  json :true
+}*/
+var tableau=new Array()
+var length=0
+request("https://www.overpass-api.de/api/interpreter?data=[out:json];node[tourism](43.46669501043081,-5.708215989569187,43.588927989569186,-5.605835010430813);out%20meta;")
+.then(response => {
+  data=JSON.parse(response)
+  //console.log(data.elements)
+  for(var i=0;i<data.elements.length;i++){
+
+    if(data.elements[i].tags.tourism!="hotel"){
+      length++
+
     }else{
-      console.log("erreur ici");
-      reject("erreur");
+      console.log("!!!!!!!!!!!!cest un hotel je n'ajoute pas")
     }
-
-  })
-}
-var fonc = () =>{
-  return new Promise(function(resolve, reject){
-    var i=1;
-    add(i)
-    .then(i => {
-
-      resolve(i);
-    })
-    .catch(err => {
-      console.log("erreur laaaa");
-      reject("erreur")
-    })
-  })
-}
-
-fonc().then(i=>{
-  console.log(i);
+  }
+  for (var i=0; i<length;i++){
+    tableau[i] = new Array()
+    tableau[i][0]=data.elements[i].lat
+    tableau[i][1]=data.elements[i].lon
+  }
+  console.log(tableau)
 })
-.catch(()=>{
-  console.log("error");
-})
+.catch(error => {
+  console.log(error)
+});
--- prorientation/src/app/trail/map/map.page.ts
+++ prorientation/src/app/trail/map/map.page.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import 'leaflet-routing-machine';
-import 'leaflet-layer-overpass';
+import 'leaflet-overpass-layer';
 declare let L;
-
-
+declare var require: any;
+import * as request from "request-promise"
 
 @Component({
   selector: 'app-map',
@@ -21,37 +21,83 @@ export class MapPage implements OnInit {
   ngOnInit() {
 
     var object = new Geolocation();
-    object.getCurrentPosition().then((resp) => {
-      var lat=resp.coords.latitude
-      var long=resp.coords.longitude
-      var mymap = L.map('mapid').setView([lat, long], 13);
-      mymap.locate({
+    //object.getCurrentPosition().then((resp) => {
+      var lat=45.75
+      var long=4.84
+      var mymap = L.map('mapid').setView([lat, long], 14);
+      /*mymap.locate({
         watch:true,
 
-      });
+      });*/
 
-      let watch = object.watchPosition();
+      /*let watch = object.watchPosition();
       watch.subscribe((data) => {
        //data can be a set of coordinates, or an error (if an error occurred).
        console.log(data.coords.latitude)
        console.log(data.coords.longitude)
        var marker = L.marker([lat, long]).addTo(mymap);
 
-     });
+     });*/
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   	    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
    	    maxZoom: 18,
    	    id: 'mapbox.streets',
    	    accessToken: 'pk.eyJ1IjoibHVjYXNib3V2YXJlbCIsImEiOiJjazJycHIwbXQwZGs3M25udmltaGg3eTFlIn0.XGIAxbBH8QGE1ZnmHUztMQ'
       }).addTo(mymap)
-      L.Routing.control({
+      //console.log(mymap.getBound())
+      /*L.Routing.control({
         waypoints: [
         L.latLng(lat, long),
         L.latLng(lat+0.4, long+0.7)
         ],
         routeWhileDragging: true
-      }).addTo(mymap);
-      L.Control.Geocoder.nominatim();
+      }).addTo(mymap);*/
+      /*var attr_osm = 'Map data &copy; <a href="http://openstreetmap.org/">OpenStreetMap</a> contributors',
+      attr_overpass = 'POI via <a href="http://www.overpass-api.de/">Overpass API</a>';
+      var osm = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {opacity: 0.7, attribution: [attr_osm, attr_overpass].join(', ')});
+
+      var map = new L.Map('map').addLayer(osm).setView(new L.LatLng(52.265, 10.524), 14);*/
+
+
+
+      var opl = new L.OverPassLayer({
+        endpoint: "http://overpass.osm.rambler.ru/cgi/",
+        query:"(node({{bbox}})[tourism];);out qt;",
+        debug: false,
+        callback: function(data) {
+          var tableau=new Array()
+          for(var i=0;i<data.elements.length;i++) {
+            tableau[i]=new Array()
+            var e = data.elements[i];
+
+            if (e.id in this.instance._ids) return;
+            console.log(e.tags.tourism)
+            if(e.tags.tourism!="hotel"){
+
+              tableau[i][0]=e.lat
+              tableau[i][1]=e.long
+              this.instance._ids[e.id] = true;
+              var pos = new L.LatLng(e.lat, e.lon);
+              var popup = this.instance._poiInfo(e.tags,e.id);
+              var color = e.tags.collection_times ? 'green':'red';
+              var circle = L.circle(pos, 50, {
+                color: color,
+                fillColor: '#fa3',
+                fillOpacity: 0.5
+              })
+              .bindPopup(popup);
+              this.instance.addLayer(circle);
+            }
+          }
+        },
+        minZoomIndicatorOptions: {
+          position: 'topright',
+          minZoomMessageNoLayer: "no layer assigned",
+          minZoomMessage: "current Zoom-Level: CURRENTZOOM all data at Level: MINZOOMLEVEL"
+        }
+      });
+      mymap.addLayer(opl);
+      //L.Control.Geocoder.nominatim();
 
 
 
@@ -62,9 +108,9 @@ export class MapPage implements OnInit {
       mymap.scrollWheelZoom.disable();
       mymap.boxZoom.disable();
       mymap.keyboard.disable();*/
-    }).catch((error) => {
+    /*}).catch((error) => {
       console.log('Error getting location', error);
-    });
+    });*/
 
 
   }
